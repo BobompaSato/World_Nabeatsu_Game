cd2211b4c27ac13ead7f1c9f2abefd55d66fe67b
Bobompasato <mendechongtai@gmail.com>
2025-10-16 00:14:32 +0900

    4r

--- nabeatsu.c
+++ nabeatsu.c
@@ -1,11 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 
 int main(void){
 
     int nabe;
     srand((unsigned int)(time(NULL)));
 
-    
+    nabe = rand();
+    printf("%d\n", nabe);
 
 }
